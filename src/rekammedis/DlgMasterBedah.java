520e588d7978fa5cfc7149e13e47e03493c1d4a7
rais <simrs.rsu.matraman@gmail.com>
2025-04-21 16:24:51 +0700

    Update OK
    laporan operasi + template

--- src/rekammedis/DlgMasterBedah.java
+++ src/rekammedis/DlgMasterBedah.java
@@ -90,7 +90,7 @@ public final class DlgMasterBedah extends javax.swing.JDialog {
         }
         tbKamar.setDefaultRenderer(Object.class, new WarnaTable());
         
-        KdP.setDocument(new batasInput((byte)6).getKata(KdP));
+        KdP.setDocument(new batasInput((byte)15).getKata(KdP));
 //        NmP.setDocument(new batasInput((byte)30).getKata(NmP));
         NmP.setDocument(new batasInput((int)2000).getKata(NmP));
         TKeluhan.setDocument(new batasInput((int)2000).getKata(TKeluhan));
@@ -118,7 +118,7 @@ public final class DlgMasterBedah extends javax.swing.JDialog {
             });
         } 
         
-        ChkInput.setSelected(false);
+        ChkInput.setSelected(true);
         isForm();  
         
     }
